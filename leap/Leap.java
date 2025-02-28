771a0900c7abdff5b1549143a1b76779acc63f2a
hactiv8rud <procord100@gmail.com>
2025-02-28 02:47:35 -0500

    edited leap.java

--- leap/Leap.java
+++ leap/Leap.java
@@ -1,14 +1,16 @@
 class Leap {
 
     boolean isLeapYear(int year) {
-        if (year % 100 == 0)
-            if (year % 400 == 0)
-                return true;
-            else
-                return false;
-        else if (year % 4 == 0)
-            return true;
-        else
-            return false;
+//        if (year % 100 == 0)
+//            if (year % 400 == 0)
+//                return true;
+//            else
+//                return false;
+//        else if (year % 4 == 0)
+//            return true;
+//        else
+//            return false;
+
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0 &);
     }
 }
